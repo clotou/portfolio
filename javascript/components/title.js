0587b45da7ecb80bff23580f7055f42e2df30d6f
clotilde lacaille <lacailleclotilde@gmail.com>
2021-07-05 10:52:16 +0200

    intro + about-me ok

---
+++ javascript/components/title.js
@@ -0,0 +1,5 @@
+// const hi = document.querySelector("#hi")
+//   hi.addEventListener("click", (event) => {
+//     event.currentTarget.classList.add("d-none")});
+
+// export { hi, clo, dev }
---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+import { hi } from "./components/title";
+
+document.querySelector("#hi").addEventListener("click", (event) => {
+  event.currentTarget.classList.add("d-none");
+});
