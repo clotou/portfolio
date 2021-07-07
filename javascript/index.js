0110c291aca3bf838c3f41f19eb366c8cbac724f
clotilde lacaille <lacailleclotilde@gmail.com>
2021-07-07 16:18:46 +0200

    index.js commenté + scrpt js commenté

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
-import { hi } from "./components/title";
+// import { hi } from "./components/title";
 
-document.querySelector("#hi").addEventListener("click", (event) => {
-  event.currentTarget.classList.add("d-none");
-});
+// document.querySelector("#hi").addEventListener("click", (event) => {
+//   event.currentTarget.classList.add("d-none");
+// });
